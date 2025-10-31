48a48776ab98935d54f27d5495c433c56b831f43
Sarah Stamper <asbp4@umsystem.edu>
2025-10-30 23:45:50 -0500

    switched from cmd.exe to powershell.exe

--- make/win.mk
+++ make/win.mk
@@ -1,23 +1,26 @@
+IFS = .
+SHELL = powershell.exe
+
 help: # print this help and exit
-	@powershell.exe -command "select-string '^[a-z]' make\\win.mk | foreach {$$_.line -replace ':[^:#]*#\s+', """`t"""}"
+	@select-string -casesensitive '^[a-z]' make\\win.mk | foreach {$$_.line -replace ':[^:#]*#\s+', "`t"}
 
 clean: # remove generated files
-	-rd /q /s bin releases
+	rm -recurse bin, releases
 
 compile: # compile program
-	-md bin
+	mkdir -force bin >$$NULL
 	robocopy.exe src bin /e /njh /njs /xf *.ui
-	mv /y bin\\main.py bin\\nudgy.py
+	mv bin\\main.py bin\\nudgy.py
 
-compile-ui: # compile ui
-	for f in $$(find src -name '*.ui' | sed 's/\.[^\/]*$$//g'); do \
-		pyuic5.exe -o "$${f}_init.py" "$$f.ui"; \
-	done
+# compile-ui: # compile ui
+# 	for f in $$(find src -name '*.ui' | sed 's/\.[^\/]*$$//g'); do \
+# 		pyuic5.exe -o "$${f}_init.py" "$$f.ui"; \
+# 	done
 
-release: compile # compile and compress program
-	-md releases
-	tar.exe -c -f releases\\main.tar.gz bin
-	mv releases/main.tar.gz "releases/main-$$(date +%s).tar.gz"
+# release: compile # compile and compress program
+# 	-md releases
+# 	tar.exe -c -f releases\\main.tar.gz bin
+# 	mv releases/main.tar.gz "releases/main-$$(date +%s).tar.gz"
 
-run: compile # compile and run program
-	bin\\nudgy.py
+# run: compile # compile and run program
+# 	bin\\nudgy.py
