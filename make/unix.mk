d84d5fac43efc2cb05171817cfefe9eec98f5a43
Sarah Stamper <asbp4@umsystem.edu>
2025-10-30 20:39:42 -0500

    added release

--- make/unix.mk
+++ make/unix.mk
@@ -2,7 +2,7 @@ help: # print this help and exit
 	@grep -E '^[a-zA-Z]' make/unix.mk | sed -E 's/:.*#\s*/\t/g' | grep -Ev '^\s*[a-zA-Z]+:\s*[a-zA-Z]+$$' | sort
 
 clean: # remove generated files
-	-rm -r bin
+	-rm -r bin releases
 
 compile: # compile program
 	mkdir -p bin
@@ -14,6 +14,11 @@ compile-ui: # compile ui
 	for f in $$(find src -name '*.ui' | sed 's/\.[^\/]*$$//g'); do \
 		pyuic5 -o "$${f}_init.py" "$$f.ui"; \
 	done
+
+release: compile # compile and compress program
+	mkdir -p releases
+	tar -c -f releases/main.tar.gz bin/
+	mv releases/main.tar.gz "releases/main-$$(date +%s).tar.gz"
 
 run: compile # compile and run program
 	bin/nudgy
