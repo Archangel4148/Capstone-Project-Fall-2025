3273466ff0c1e2d38e2f514f33f551226a8918ca
Ryan Brazill <rjbw3c@umsystem.edu>
2025-10-30 20:43:11 -0500

    Fixed the function locations

--- src/tabs/to_do_list_tab.py
+++ src/tabs/to_do_list_tab.py
@@ -10,16 +10,13 @@ class ToDoListTab(BaseNudgyTab):
     def __init__(self, parent_tab_widget: QTabWidget):
         super().__init__(parent_tab_widget)
 
+        self.ui.add_task_button.clicked.connect(lambda: self.addItem())
+        self.ui.remove_task_button.clicked.connect(lambda: self.deleteItem())
 
-        def addItem(self):
-            item = self.ui.task_description_line_edit.text() + ' Due: ' + self.ui.due_date_time_edit.text()
-            self.ui.listWidget.addItem(item)
+    def addItem(self):
+        item = self.ui.task_description_line_edit.text() + ' Due: ' + self.ui.due_date_time_edit.text()
+        self.ui.listWidget.addItem(item)
 
-        def deleteItem(self):
-            clicked = self.ui.listWidget.currentRow()
-            self.ui.listWidget.takeItem(clicked)
-
-        self.ui.add_task_button.clicked.connect(lambda: addItem(self))
-        self.ui.remove_task_button.clicked.connect(lambda: deleteItem(self))
-
-        
+    def deleteItem(self):
+        clicked = self.ui.listWidget.currentRow()
+        self.ui.listWidget.takeItem(clicked)
