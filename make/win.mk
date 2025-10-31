1c2d39b4213401f2944973cb4143f7ff72413c16
Sarah Stamper <asbp4@umsystem.edu>
2025-10-30 21:26:07 -0500

    added part of windows make

--- make/win.mk
+++ make/win.mk
@@ -1,2 +1,23 @@
-help:
+help: # print this help and exit
 	@powershell.exe -command "select-string '^[a-z]' make\\win.mk | foreach {$$_.line -replace ':[^:#]*#\s+', """`t"""}"
+
+clean: # remove generated files
+	-rd /q /s bin releases
+
+compile: # compile program
+	-md bin
+	robocopy.exe src bin /e /njh /njs /xf *.ui
+	mv /y bin\\main.py bin\\nudgy.py
+
+compile-ui: # compile ui
+	for f in $$(find src -name '*.ui' | sed 's/\.[^\/]*$$//g'); do \
+		pyuic5.exe -o "$${f}_init.py" "$$f.ui"; \
+	done
+
+release: compile # compile and compress program
+	-md releases
+	tar.exe -c -f releases\\main.tar.gz bin
+	mv releases/main.tar.gz "releases/main-$$(date +%s).tar.gz"
+
+run: compile # compile and run program
+	bin\\nudgy.py
