c88b7654ec566bd4d0eb92775f0f9980def4c63f
Sarah Stamper <asbp4@umsystem.edu>
2025-11-24 20:29:13 -0600

    moved timer api to its own file

--- src/tabs/timer_tab.py
+++ src/tabs/timer_tab.py
@@ -1,8 +1,7 @@
-import dataclasses
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QTabWidget
 
-from api.database_service import DatabaseService
+from api.timer import TimerTabAPI
 from tabs.base_tab import BaseNudgyTab
 from ui.timer_tab_init import Ui_timer_tab
 
@@ -118,24 +117,3 @@ class TimerTab(BaseNudgyTab):
         self.timer_value = self.start_time
         self.update_display()
         self.start_stop_timer()
-
-@dataclasses.dataclass
-class Timer:
-    duration_sec: float
-
-class TimerTabAPI:
-    def get_all_timers(self) -> list[Timer]:
-        # Select all rows from the database
-        rows = DatabaseService.select(table_name="timer", columns=None, conditions=None)
-        # Build the Timer objects
-        timers = [Timer(*row) for row in rows]
-        return timers
-
-
-    def delete_timer(self, selected_timer: Timer):
-        # Delete the selected timer from the database
-        DatabaseService.delete(table_name="timer", conditions=[("timer_id", "=", selected_timer.duration_sec)])
-
-    def add_timer(self, timer: Timer):
-        # Add the provided Timer to the database
-        DatabaseService.insert(table_name="timer", values={"duration": timer.duration_sec})
