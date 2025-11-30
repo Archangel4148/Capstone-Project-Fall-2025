06f7ebc8f6a867e4626827484a50652c198c4c1f
Sarah Stamper <asbp4@umsystem.edu>
2025-11-29 23:02:13 -0600

    added protections for root programs being focused

--- src/linux/x11/app_tracker.py
+++ src/linux/x11/app_tracker.py
@@ -8,6 +8,7 @@ import subprocess
 
 def get_active_window_linux_x11() -> str:
     # doesn't support wayland since there isn't a consistent method for doing so that's also supported by multiple compositors.
+    # doesn't support flatpak since it runs in a protected environment.
     proc = subprocess.Popen(["xdotool", "getactivewindow"], stdout=subprocess.PIPE).stdout.read()
     encoding = chardet.detect(proc)["encoding"]
     proc = str(proc, encoding=encoding).strip()
@@ -17,8 +18,14 @@ def get_active_window_linux_x11() -> str:
     pid = str(proc, encoding=encoding).strip()
     pid = int(pid)
 
-    path = psutil.Process(pid).exe()
-    return os.path.realpath(path)
+    path = ""
+    try:
+        path = psutil.Process(pid).exe()
+        path = os.path.realpath(path)
+    except psutil.AccessDenied:
+        pass
+
+    return path
 
 class MatchExeToDesktop():
     def __init__(self, exe_path: str) -> None:
