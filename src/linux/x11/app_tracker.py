3066437556ae10e6e809a62178614575fc495397
Sarah Stamper <asbp4@umsystem.edu>
2025-11-29 22:47:10 -0600

    added correct name output to file

--- src/linux/x11/app_tracker.py
+++ src/linux/x11/app_tracker.py
@@ -25,14 +25,21 @@ class MatchExeToDesktop():
         self._exe_path = exe_path
         self._result = os.path.basename(exe_path)
 
-    def _search_desktop_file(self, file_path: str) -> bool:
+    def _result_found(self) -> bool:
+        return self._exe_path == self._result
+
+    def _search_desktop_file(self, file_path: str) -> None:
         config = configparser.ConfigParser()
         config.read(file_path)
 
+        exec = ""
         try:
-            print(config.get("Desktop Entry", "Exec", raw=True))
+            exec = config.get("Desktop Entry", "Exec", raw=True).split()
         except configparser.NoOptionError:
             pass
+
+        if self._exe_path in exec or os.path.basename(self._exe_path) in exec:
+            self._result = config.get("Desktop Entry", "Name", raw=True)
 
     def _search_desktop_files(self, dir: str) -> None:
         for f in glob.glob(f"{dir}/**", recursive=True):
@@ -41,8 +48,16 @@ class MatchExeToDesktop():
 
             self._search_desktop_file(f)
 
+            if self._result_found():
+                break
+
     def main(self) -> None:
         for d in os.environ["XDG_DATA_DIRS"].split(":"):
             d = f"{d}/applications"
 
             self._search_desktop_files(d)
+
+            if self._result_found():
+                break
+
+        return self._result
